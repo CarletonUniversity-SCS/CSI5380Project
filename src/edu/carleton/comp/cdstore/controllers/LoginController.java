2be32955d56b1aa8edd39f738007ba56a6e9341a
vincentwang <vincentwang901130@me.com>
2015-10-12 21:26:35 -0400

    logincontroller added

---
+++ src/edu/carleton/comp/cdstore/controllers/LoginController.java
@@ -0,0 +1,11 @@
+package edu.carleton.comp.cdstore.controllers;
+
+import javax.servlet.http.HttpServlet;
+
+public class LoginController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	private void doGet()
+	
+	
+}

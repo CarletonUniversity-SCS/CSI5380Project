f77a9c55a9ae0674a14b10f917ead42a26a2b8bd
vincentwang <vincentwang901130@me.com>
2015-10-14 09:09:07 -0400

    CDDAO

---
+++ src/edu/carleton/comp/cdstore/dao/CDDAO.java
@@ -0,0 +1,67 @@
+package edu.carleton.comp.cdstore.dao;
+
+import java.sql.ResultSet;
+
+public class CDDAO extends DAO{
+
+	@Override
+	public boolean create(Object paramObject) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Object paramObject) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int paramInt) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(String paramString) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Object paramObject) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteAll() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object findByPrimaryKey(int paramInt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object findByPrimaryKey(String paramString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object find(Object paramObject) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object getDataObject(ResultSet paramResultSet, boolean paramBoolean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

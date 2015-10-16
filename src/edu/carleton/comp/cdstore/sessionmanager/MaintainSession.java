d8281530a86c7996ed6a9a4a6154a339444ced51
ZhiboZhang <zhibo_zhang0914@yahoo.com>
2015-10-16 11:03:01 -0400

    Modify maintain session controller and mapping in web.xml.

--- src/edu/carleton/comp/cdstore/sessionmanager/MaintainSession.java
+++ src/edu/carleton/comp/cdstore/sessionmanager/MaintainSession.java
@@ -13,18 +13,29 @@ import javax.servlet.http.HttpSession;
 
 import com.alibaba.fastjson.JSON;
 
+import edu.carleton.comp.cdstore.dao.CustomerDAO;
+import edu.carleton.comp.cdstore.models.Customer;
+
 public class MaintainSession extends HttpServlet{
 	private static final long serialVersionUID = 1L;
+	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		req.setCharacterEncoding("utf-8");
 		HttpSession session=req.getSession();
-		String email=(String)session.getAttribute("account");
-		resp.setContentType("text/html;charset=UFT-8");
 		PrintWriter out=resp.getWriter();
+		String email=(String)session.getAttribute("account");
+		if(email!=null){
+		CustomerDAO dao=new CustomerDAO();
+		Customer customer=(Customer)dao.findByPrimaryKey(email);
 		Map<String, Object> result=new HashMap<String,Object>();
-		result.put("account",email);
+		result.put("account",customer);
 		String resultstring=JSON.toJSONString(result);
 		out.print(resultstring);	
+		}else{
+			out.print("null");
+		}
 	}
 	
 	public static long getSerialversionuid() {
